31cbbc5ba7d9baebfea2aa89c09a00884941260e
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-12-15 00:34:03 +0200

    Домашка

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum Days {
+  Monday = "Monday",
+  Tuesday= "Tuesday",
+  Wednesday= "Wednesday",
+  Thursday= "Thursday",
+  Friday= "Friday",
+  Saturday= "Saturday",
+  Sunday= "Sunday",
+}
+
+function isWeekend(day: Days):boolean {
+  
+ return day === Days.Saturday || day === Days.Sunday
+ 
+}
+
